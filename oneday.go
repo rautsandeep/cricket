4095e18d554a5b101b648dbf8159ce6822882788
Sandeep Raut <rautsandeep0@gmail.com>
2025-12-06 17:55:02 +0530

    second

---
+++ oneday.go
@@ -0,0 +1,15 @@
+packge main
+
+import(
+"fmt"
+"github.com/sandeepraut/alpha/dog"
+)
+
+func main(){
+ fmt.Println("This is from the cricket package")
+ s:= dog.Doggy()
+ fmt.Println("the value of function from the dog",s)
+
+}
+
+
